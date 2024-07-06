8c85d03f3819e8f7b0df92eb5523ea8560cbc012
hamoodhabibi235 <salilpat5428@k12.ipsd.org>
2024-07-05 21:19:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+function convertTime() {
+var ovenTemp = document.getElementById('ovenTemp').value;
+var ovenTime = document.getElementById('ovenTime').value;
 
+if (ovenTemp && ovenTime) {
+var airFryerTemp = ovenTemp - 25;
+var airFryerTime = ovenTime * 0.8;
+
+document.getElementById('result').innerHTML =
+"Air Fryer Temperature: " + airFryerTemp + "°F<br>" +
+"Air Fryer Time: " + Math.round(airFryerTime) + " minutes";
+} else {
+document.getElementById('result').innerHTML = "Please enter both oven temperature and time.";
+}
+}
